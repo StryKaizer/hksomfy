9212751da013b9edc28445650787d3c85219242f
Jimmy Henderickx <jimmyhdx@gmail.com>
2016-11-14 22:50:59 +0100

    Initial commit: none working state

---
+++ README.md
@@ -0,0 +1,9 @@
+# hksomfy
+
+Somfy RTS implementation using [HomeControl](https://github.com/brutella/hc).
+
+# Installation
+
+## Go
+
+Make sure that you have a working [Go installation](http://golang.org/doc/install).
---
+++ hksomfy.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"strconv"
+
+	"github.com/brutella/hc"
+	"github.com/brutella/hc/accessory"
+	"github.com/strykaizer/somfy/somfaccessory"
+)
+
+func main() {
+	info := accessory.Info{
+		Name:         "Blinds", // Do not use spaces here, get transformed to _.
+		SerialNumber: "1.0",
+		Model:        "Somfy RTS",
+		Manufacturer: "Jimmy Henderickx",
+	}
+
+	acc := somfaccessory.NewWindowCovering(info)
+
+	acc.WindowCovering.CurrentPosition.OnValueRemoteUpdate(func(currentposition int) {
+		log.Println("CurrentPosition" + strconv.Itoa(currentposition))
+	})
+
+	acc.WindowCovering.TargetPosition.OnValueRemoteUpdate(func(targetposition int) {
+		log.Println("TargetPosition" + strconv.Itoa(targetposition))
+		acc.WindowCovering.CurrentPosition.SetValue(100) // TODO
+	})
+
+	t, err := hc.NewIPTransport(hc.Config{Pin: "11111111"}, acc.Accessory)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hc.OnTermination(func() {
+		t.Stop()
+	})
+
+	t.Start()
+}
